0f662b9c35c889fab344069c9e60790c494b4d82
HaraTeja45 <123100502+HaraTeja45@users.noreply.github.com>
2025-02-01 20:02:01 +0530

    Initial Commit

---
+++ food-order-management-service/src/main/java/com/cp/foodordermanagement/FoodOrderManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cp.foodordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderManagementServiceApplication.class, args);
+	}
+
+}
---
+++ food-order-management-service/src/test/java/com/cp/foodordermanagement/FoodOrderManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp.foodordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
