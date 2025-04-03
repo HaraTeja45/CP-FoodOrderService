57c61af786e41d2d0919f45a815b7f105c2b6dbd
Venkata Hara Teja <venkata.teja@bajajfinserv.in>
2025-04-03 14:40:15 +0530

    payment related changes

--- food-order-management-service/src/main/java/com/cp/foodordermanagement/model/MasterOrderDetails.java
+++ food-order-management-service/src/main/java/com/cp/foodordermanagement/model/MasterOrderDetails.java
@@ -8,8 +8,9 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -18,6 +19,7 @@ import jakarta.persistence.Table;
 public class MasterOrderDetails {
 
 	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long masterOrderId;
 
 	@OneToMany(mappedBy = "masterOrderDetails",cascade = CascadeType.ALL)
--- food-order-management-service/src/main/java/com/cp/foodordermanagement/model/OrderDetails.java
+++ food-order-management-service/src/main/java/com/cp/foodordermanagement/model/OrderDetails.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,6 +16,7 @@ import jakarta.persistence.Table;
 public class OrderDetails {
 
 	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long orderId;
 
 	private Long restaurantKey;
